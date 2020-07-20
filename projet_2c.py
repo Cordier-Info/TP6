d1cb6388a5818f4119b6baafbd76a08f9874ef9f
Prof Info <66843772+Cordier-Info@users.noreply.github.com>
2020-07-20 16:01:17 +0200

    Create projet_2c.py

---
+++ projet_2c.py
@@ -0,0 +1,11 @@
+# méthode pour écrire un fichier en binaire :
+
+f = open('imageNB_comp.dat','wb')
+# s : chaîne de caractère obtenue en remplaçant chaque caractère
+# du fichier original par son code obtenu grâce à l'arbre
+s = '1001110011...' 
+# b est la transcription de ce code en bytes
+b = bytes(int(s[i : i + 8], 2) for i in range(0, len(s), 8))
+# puis on l'écrit dans f
+f.write(b)
+f.close()
